1f0cd2a66cf7000747048975d9756b9aeadb4431
gjcomputer <gjcomputer@naver.com>
2019-04-10 16:13:28 +0900

    Phone management Beta 6.0 Design change Data function and change Menu screen

--- phone_management.c
+++ phone_management.c
@@ -17,7 +17,7 @@ typedef struct __person{
 person pList[50];
 int perNum=0;
 
-enum {ADD=1, SEARCH=2, DEL=3, SHOWALL=4, END=0};
+enum {ADD=1, SEARCH=2, DEL=3, SHOWALL=4, CHANGE=5, END=0};
 
 void clearbuf(void){while(getchar()!='\n'){}}
 
@@ -26,9 +26,11 @@ void addData(void);
 void searchData(void);
 void delData(void);
 void ShowAllData(void);
+void ChangeData(void);
 
 void ShowPersonData(person ref);
 void DelPersonData(int idx);
+void changePersonData(int idx);
 
 void SaveData(void);
 void LoadData(void);
@@ -51,6 +53,8 @@ int main()
              delData(); break;
         case SHOWALL:
             ShowAllData(); break;
+        case CHANGE:
+            ChangeData();break;
         }
         sw=ShowMenu();
     }
@@ -66,6 +70,8 @@ int ShowMenu(void)
     puts("2. 검          색 ");
     puts("3. 삭          제 ");
     puts("4. 전  체   목 록  ");
+    puts("5. 수          정");
+    puts("0. 프 로 그 램 종 료");
     puts("-------------------------");
     printf("선택 : ");scanf("%d",&n);
     clearbuf();
@@ -125,7 +131,7 @@ void searchData(void)
 
 void delData(void)
 {
-    int idx=0,i=0,j=0;
+    int idx=0,i=0;
     int idxMatch[50]={0,};
     char sName[NAME_LEN];
     printf("삭제 할 이름 : ");gets(sName);
@@ -138,7 +144,6 @@ void delData(void)
             i++;
         }
     }
-    j=i;
 
     for(idx=0;idx<i;idx++)
     {
@@ -166,6 +171,33 @@ void ShowAllData(void)
     clearbuf();
 }
 
+void ChangeData(void)
+{
+    int idx=0,i=0;
+    int idxMatch[50]={0,};
+    char sName[NAME_LEN];
+    printf("수정 할 이름 : ");gets(sName);
+
+    for(idx=0;idx<perNum;idx++)
+    {
+        if(!strcmp(sName,pList[idx].name))
+        {
+            idxMatch[i]=idx;
+            i++;
+        }
+    }
+
+    for(idx=0;idx<i;idx++)
+    {
+        printf("No. %d \n",idx+1);
+        ShowPersonData(pList[idxMatch[idx]]);
+    }
+
+
+    printf("선택 : ");scanf("%d",&idx);clearbuf();
+    changePersonData(idxMatch[idx-1]);
+}
+
 void ShowPersonData(person ref)
 {
     printf("┌──────────────────────────────\n");
@@ -188,6 +220,36 @@ void DelPersonData(int idx)
     perNum--;
     SaveData();
     puts("삭제가 완료 되었습니다.");
+}
+
+void changePersonData(int idx)
+{
+    person buf;
+    int ref=0;
+    int i=0;
+
+    printf("이   름 : ");gets(buf.name);
+    printf("전화번호 : ");gets(buf.pNum);
+    printf("메   모 : ");gets(buf.memo);
+
+    for(i=0;i<perNum;i++)
+    {
+        if(!strcmp(buf.name,pList[i].name) && !strcmp(buf.pNum,pList[i].pNum))
+        {
+            printf("연락처가 이미 존재합니다.");getchar();
+            return;
+         }
+    }
+    printf("연락처를 변경 합니다. (1. 확인, 0.취소) : ");
+    scanf("%d",&ref);
+
+    if(ref==1)
+    {
+        pList[idx]=buf;
+        SaveData();
+    }
+
+    clearbuf();
 }
 
 
