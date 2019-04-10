4fa4bb0a656d5b853d9331f2f3bb3796ab89bfa8
gjcomputer <gjcomputer@naver.com>
2019-04-10 11:16:25 +0900

    Phone Management Beta 3.0 Design Save and Load Function

--- phone_management.c
+++ phone_management.c
@@ -25,9 +25,14 @@ void searchData(void);
 void delData(void);
 void ShowAllData(void);
 
+void SaveData(void);
+void LoadData(void);
+
 int main()
 {
     int sw;
+    LoadData();
+
     sw=ShowMenu();
     while(sw!=END)
     {
@@ -67,10 +72,23 @@ int ShowMenu(void)
 void addData(void)
 {
     puts("연락처 추가를 시작합니다.");
-    printf("이   름 : ");gets(pList[perNum].name);
-    printf("전화번호 : ");gets(pList[perNum].pNum);
-    printf("메   모 : ");gets(pList[perNum].memo);
-    perNum++;
+    person buf;
+    int ref;
+
+    printf("이   름 : ");gets(buf.name);
+    printf("전화번호 : ");gets(buf.pNum);
+    printf("메   모 : ");gets(buf.memo);
+
+    printf("연락처를 추가 합니다. (1. 확인, 0.취소) : ");
+    scanf("%d",&ref);
+
+    if(ref==1)
+    {
+        pList[perNum]=buf;
+        perNum++;
+        SaveData();
+    }
+
     clearbuf();
 }
 
@@ -98,4 +116,32 @@ void ShowAllData(void)
         printf("└─────────────────────────────\n");
     }
     clearbuf();
+}
+
+void SaveData(void)
+{
+    int idx;
+    FILE* fps=fopen("PhoneData.bin","wb");
+
+    fwrite(&perNum,sizeof(int),1,fps);
+    for(idx=0;idx<perNum;idx++)
+    {
+        fwrite(&pList[idx],sizeof(person),1,fps);
+    }
+    fclose(fps);
+}
+
+void LoadData(void)
+{
+    int idx;
+
+    FILE* fpr=fopen("PhoneData.bin", "rb");
+
+    fread(&perNum,sizeof(int),1,fpr);
+    for(idx=0;idx<perNum;idx++)
+    {
+        fread(&pList[idx],sizeof(person),1,fpr);
+    }
+    fclose(fpr);
+
 }
